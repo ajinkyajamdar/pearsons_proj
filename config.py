abf3770e5d5b767301760e0ff1f7bd847974dea9
unknown <ajinkya.jamdar7@gmail.com>
2023-03-31 20:50:36 +0530

    Added all reqd data

---
+++ config.py
@@ -0,0 +1,4 @@
+
+
+MODEL_FILE_PATH = r'C:\Ajinkya Python\Ashish Sir Notes\API for medical Insurance\Ajinkya Medical Insurance Prediction (HTML to API)\Medical Insurance\project_app\Linear_Model.pkl'
+JSON_FILE_PATH = r'C:\Ajinkya Python\Ashish Sir Notes\API for medical Insurance\Ajinkya Medical Insurance Prediction (HTML to API)\Medical Insurance\project_app\project_data.json'
